1aaf7af2f6aaa1f3471f1d1e65606f350def9248
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-10-03 17:08:33 +0100

    add vite, update docker settings, other initial setup

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "frontend",
+  build: {
+    Outdir: "dist",
+    manifest: "manifest.json",
+    rollupOptions: {
+      input: "frontend/index.js",
+    },
+  },
+});
