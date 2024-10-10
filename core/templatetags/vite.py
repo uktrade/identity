e1d4c5307aee1af2b5b56ce15b6922731947e3c3
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-10-10 09:32:17 +0100

    IDPF-87 Create boilerplate code structure (#1)

---
+++ core/templatetags/vite.py
@@ -0,0 +1,57 @@
+import json
+import logging
+
+from django import template
+from django.conf import settings
+from django.templatetags.static import static
+from django.utils.safestring import mark_safe
+
+
+logger = logging.getLogger(__name__)
+
+register = template.Library()
+
+
+def load_manifest() -> dict:
+    with settings.VITE_MANIFEST_PATH.open() as f:
+        manifest = json.load(f)
+
+    logger.debug("Vite manifest loaded")
+
+    return manifest
+
+
+manifest = load_manifest()
+
+
+@register.simple_tag(takes_context=True)
+def vite_css(context, filename: str) -> str:
+    if settings.VITE_DEV:
+        return ""
+
+    return mark_safe(
+        f'<link rel="stylesheet" href="{static(manifest[filename]["file"])}">'
+    )
+
+
+@register.simple_tag(takes_context=True)
+def vite_js(context, filename: str) -> str:
+    if settings.VITE_DEV:
+        return ""
+
+    return mark_safe(
+        f'<script type="module" src="{static(manifest[filename]["file"])}"></script>'
+    )
+
+
+@register.simple_tag(takes_context=True)
+def vite_local(context, filename: str) -> str:
+    if not settings.VITE_DEV:
+        return ""
+
+    scripts = [
+        f'<script type="module" src="{settings.VITE_DEV_SERVER_URL}/@vite/client"></script>',
+        f'<script type="module" src="{settings.VITE_DEV_SERVER_URL}/{filename}"></script>',
+    ]
+
+    return mark_safe("\n".join(scripts))
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+hide:
+  - navigation
+---
+
+# About
+
+Identity is the dedicated service for staff profile and directory information for the Department for Business and Trade (DBT) built using [Django](https://www.djangoproject.com/).
