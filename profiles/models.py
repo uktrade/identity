d3fdc9a19695b83cf92dc040a11f7045ad5d4f1e
Ryan Williams <ryan.williams2@digital.trade.gov.uk>
2024-10-22 17:02:56 +0100

    well at least it builds...

--- profiles/models.py
+++ profiles/models.py
@@ -2,16 +2,39 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-class FieldAuthoritativenessMixin():
-    def __init__(self,*args, **kwargs):
-        if "is_authoritative" not in kwargs:
-            raise TypeError()
-        
-        self.is_authoritative = kwargs['is_authoritative']
-        super(FieldAuthoritativenessMixin,self).__init__(*args,**kwargs)
+# 1,
+# list_auth_fields = []
+# if field in list:
+#     then it's a super cool field
+# else:
+#     boring regular field
 
-class CharField(models.CharField,FieldAuthoritativenessMixin):
-    ...
+# 2,
+# if field.is_authoritative:
+#       then it's a super cool field
+# else:
+#       boring regular field
+
+
+class FieldAuthoritativenessMixin:
+    is_authoritative: bool = False
+
+    def __init__(self, is_authoritative, *args, **kwargs):
+        self.is_authoritative = bool(kwargs.pop("is_authoritative", False))
+
+
+class AuthoritativeCharField(models.CharField):
+    is_authoritative: bool = False
+    def __init__(self, *args, **kwargs):
+        self.is_authoritative = bool(kwargs.pop("is_authoritative", False))
+        # super(FieldAuthoritativenessMixin, self).__init__(
+        #     *args, **kwargs
+        # ),
+
+        super().__init__(
+            *args,
+            **kwargs,
+        ),
 
 
 class AbstractProfile(models.Model):
@@ -25,7 +48,18 @@ class AbstractProfile(models.Model):
 
 
 class PeopleFinderProfile(AbstractProfile):
-    fav_progarm = CharField(max_length=255, null=True)
+    fav_program = AuthoritativeCharField(
+        verbose_name="fav_program",
+        max_length=255,
+        null=True,
+        is_authoritative=False,
+    )
+    super_important = AuthoritativeCharField(
+        verbose_name="super_important",
+        max_length=255,
+        null=True,
+        is_authoritative=True,
+    )
     team = models.ForeignKey(
         "PeopleFinderTeam",
         on_delete=models.SET_NULL,
