182162c3f039c9d08947c56d64074597c059adc6
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-11-04 12:43:15 +0000

    wip - test ninja

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from .api import api
 
 
 urlpatterns = [
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
+    path("api/", api.urls),
 ]
