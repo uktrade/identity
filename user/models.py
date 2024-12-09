fb4948250fdc6fe55f6c1d0d034148e50e511e70
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-12-09 17:21:01 +0000

    update user manager so we can create users via manage.py

--- user/models.py
+++ user/models.py
@@ -1,13 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .managers import UserManager
+
 
 # Create your models here.
 
 
 class User(AbstractUser):
     sso_email_id = models.CharField(primary_key=True, unique=True)
+
+    objects = UserManager()
+
     USERNAME_FIELD = "sso_email_id"
+    REQUIRED_FIELDS: list[str]
 
     def __str__(self):
         return self.sso_email_id
