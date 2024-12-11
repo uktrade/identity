4e8d659ebbdaddbe769c2aafac7f84ec36d197fa
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-12-11 11:26:38 +0000

    remove user model changes

--- user/models.py
+++ user/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .managers import UserManager
 
 
 # Create your models here.
@@ -9,11 +8,7 @@ from .managers import UserManager
 
 class User(AbstractUser):
     sso_email_id = models.CharField(primary_key=True, unique=True)
-
-    objects = UserManager()
-
     USERNAME_FIELD = "sso_email_id"
-    REQUIRED_FIELDS: list[str]
 
     def __str__(self):
         return self.sso_email_id
