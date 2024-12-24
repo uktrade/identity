4a5c6437b26cd5d85573299f215da27a0a007d55
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-12-24 16:08:02 +0000

    update create from sso function

--- profiles/services/__init__.py
+++ profiles/services/__init__.py
@@ -1,17 +1,24 @@
 # This is the entrypoint service that coordinates between the sub-services
 # If in doubt about what to use, you should probably be using this
 
+from profiles.models.generic import Profile
 from profiles.services import combined, staff_sso
 from user.models import User
 
 
-def generate_combined_profile_fields(sso_email_id: str):
+def generate_combined_profile_data(sso_email_id: str):
     """
     Figures out which info we have in specific profiles and runs through the
     field hierarchy per data type to generate the values for the combined.
     create method
     """
     raise NotImplementedError()
+    return {
+        "first_name": "",
+        "last_name": "",
+        "preferred_email": "",
+        "emails": [],
+    }
 
 
 def create_from_sso(
@@ -22,20 +29,28 @@ def create_from_sso(
     preferred_email: str | None = None,
 ):
     """A central function to create all relevant profile details"""
-    combined.create(
-        sso_email_id,
-        first_name,
-        last_name,
-        emails,
-        preferred_email,
-    )
     staff_sso.create(
         sso_email_id,
         first_name,
         last_name,
         emails,
     )
-    # staff_sso.create()
-    # payload = generate_combined_profile_values()
-    # combined.create(sso_user_id, **payload)
-    raise NotImplementedError()
+    combined_profile_data = generate_combined_profile_data()
+    try:
+        profile = combined.get_by_id(sso_email_id)
+    except Profile.DoesNotExist:
+        combined.create(
+            sso_email_id,
+            combined_profile_data["first_name"],
+            combined_profile_data["last_name"],
+            combined_profile_data["preferred_email"],
+            combined_profile_data["emails"],
+        )
+    else:
+        combined.update(
+            profile,
+            combined_profile_data["first_name"],
+            combined_profile_data["last_name"],
+            combined_profile_data["preferred_email"],
+            combined_profile_data["emails"],
+        )
