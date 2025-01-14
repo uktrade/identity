e841ed6dc147edfdd0774d8f4d2b2b5288e6509c
Ryan Williams <ryan.williams2@digital.trade.gov.uk>
2025-01-14 12:27:27 +0000

    mege with main

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from core.api.scim import router as scim_router
 from core.api.sso_profile import router as sso_profile_router
 
 
-protected_apis.add_router("scim/v2/Users", scim_router)
+protected_apis.add_router("/scim/v2/Users", scim_router)
 protected_apis.add_router("/sso", sso_profile_router)
 
 urlpatterns = [
