cfb9d431a3bf9da5a971c15b7370a461abe2eec4
Ryan Williams <ryan.williams2@digital.trade.gov.uk>
2025-01-14 12:22:55 +0000

    added pre-fix '/' to sso

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from scim.api import router as scim_router
 
 
 protected_apis.add_router("scim/v2/Users", scim_router)
-protected_apis.add_router("sso/", sso_profile_router)
+protected_apis.add_router("/sso", sso_profile_router)
 
 urlpatterns = [
     path("", include("core.urls")),
