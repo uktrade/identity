637f6aef21162d919e3cd426d5af56b00fbafeb6
marcelkornblum <me@marcelkornblum.com>
2025-01-15 17:36:31 +0000

    move config to core.api

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,7 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 from core.api import protected_apis
-from core.api.scim import router as scim_router
-from core.api.sso_profile import router as sso_profile_router
-
-
-if settings.APP_ENV == "local" or settings.INFRA_SERVICE == "SSO_SCIM":
-    protected_apis.add_router("/scim/v2/Users", scim_router)
-if settings.APP_ENV == "local" or settings.INFRA_SERVICE == "SSO_PROFILE":
-    protected_apis.add_router("/sso", sso_profile_router)
 
 urlpatterns = [
     path("", include("core.urls")),
--- core/api/__init__.py
+++ core/api/__init__.py
@@ -3,6 +3,9 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django_hawk.utils import DjangoHawkAuthenticationFailed, authenticate_request
 from ninja import NinjaAPI
 
+from core.api.scim import router as scim_router
+from core.api.sso_profile import router as sso_profile_router
+
 
 def do_hawk_auth(request):
     try:
@@ -17,3 +20,9 @@ protected_apis = NinjaAPI(
     auth=do_hawk_auth,
     docs_decorator=staff_member_required,
 )
+
+
+if settings.APP_ENV == "local" or settings.INFRA_SERVICE == "SSO_SCIM":
+    protected_apis.add_router("/scim/v2/Users", scim_router)
+if settings.APP_ENV == "local" or settings.INFRA_SERVICE == "SSO_PROFILE":
+    protected_apis.add_router("/sso", sso_profile_router)
