a785df6a6f2d4a916f653fe84aed2716b21fc535
marcelkornblum <me@marcelkornblum.com>
2025-01-16 01:53:22 +0000

    all files added

---
+++ core/api/people_finder.py
@@ -0,0 +1,26 @@
+from ninja import Router
+
+from core import services as core_services
+from core.schemas import Error
+from core.schemas.profiles import ProfileMinimal
+from profiles.models.combined import Profile
+
+
+router = Router()
+
+
+@router.get(
+    "{id}",
+    response={
+        200: ProfileMinimal,
+        404: Error,
+    },
+)
+def get_user(request, id: str):
+    """Returns the combined Profile"""
+    try:
+        return core_services.get_by_id(id)
+    except Profile.DoesNotExist:
+        return 404, {
+            "message": "Unable to find user",
+        }
