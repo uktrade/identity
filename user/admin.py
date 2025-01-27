15bea69a0b04655a025a6ce57701746fa016416a
Oyeniyi Oyelade <oyeniyi.oyelade@digital.trade.gov.uk>
2025-01-27 16:34:50 +0000

    added bulk sync ID with sso to Django Admin page

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
+from core.services import sync_bulk_sso_users
 from user.models import User
 
 
-# Register your models here.
-admin.site.register(User)
+# # Register your models here.
+
+@admin.register(User)
+class SSOSyncUserAdmin(admin.ModelAdmin):
+    actions = ["sync_sso_users"]
+    @admin.action(description="Sync identity users with Staff SSO")
+    def sync_sso_users(self, request, queryset) -> None:
+        sync_bulk_sso_users()
