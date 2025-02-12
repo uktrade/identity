abdffb4c447b7323f1b4c9bd3625b4aff3b00b6f
Oyeniyi Oyelade <oyeniyi.oyelade@digital.trade.gov.uk>
2025-02-12 15:22:57 +0000

    adding delete people finder profile service and tests.

--- profiles/services/__init__.py
+++ profiles/services/__init__.py
@@ -2,9 +2,9 @@
 # If in doubt about what to use, you should probably be using this
 from typing import Optional
 
+from django.contrib.admin.models import DELETION, LogEntry
 from django.db import models
 
-from django.contrib.admin.models import DELETION, LogEntry
 from profiles.models.combined import Profile
 from profiles.services import combined, peoplefinder, staff_sso
 from profiles.types import Unset
