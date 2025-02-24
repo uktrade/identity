4769d6ee2bcd318864eb34ba5716f415a2cf3eb1
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-02-24 11:23:26 +0000

    wip

--- core/api/people_finder.py
+++ core/api/people_finder.py
@@ -2,13 +2,18 @@ from ninja import Router
 
 from core import services as core_services
 from core.schemas import Error
-from core.schemas.profiles import ProfileMinimal
+from core.schemas.profiles import ProfileMinimal, UkStaffLocationSchema
 from profiles.models.combined import Profile
+from profiles.models.generic import UkStaffLocation
+from profiles.models.peoplefinder import PeopleFinderProfile
+from profiles.services import peoplefinder
 
 
 router = Router()
 profile_router = Router()
+uk_location_router = Router()
 router.add_router("person", profile_router)
+router.add_router("uk-location", uk_location_router)
 
 
 # NB this is a placeholder to get the router running, it may need editing or deleting etc.
@@ -26,4 +31,26 @@ def get_user(request, id: str):
     except Profile.DoesNotExist:
         return 404, {
             "message": "Unable to find user",
+        }
+
+@uk_location_router.get(
+    "{id}",
+    response={
+        200: UkStaffLocationSchema,
+        404: Error,
+    }
+)
+def get_uk_staff_location(request, slug: str) -> tuple[int, UkStaffLocation | dict]:
+    try:
+        # Use get by slug from core
+        profile = peoplefinder.get_by_slug(slug=slug)
+        location = profile.uk_office_location.__dict__
+        return 200, location
+    except PeopleFinderProfile.DoesNotExist:
+        return 404, {
+            "message": "People finder profile does not exist",
+        }
+    except AttributeError:
+        return 404, {
+            "message": "Uk staff location is not set for the people finder profile",
         }
--- core/schemas/profiles.py
+++ core/schemas/profiles.py
@@ -1,6 +1,7 @@
 from ninja import Field, ModelSchema
 
 from profiles.models.combined import Profile
+from profiles.models.generic import UkStaffLocation
 
 
 class ProfileMinimal(ModelSchema):
@@ -14,4 +15,14 @@ class ProfileMinimal(ModelSchema):
             "primary_email",
             "contact_email",
             "emails",
+        ]
+
+class UkStaffLocationSchema(ModelSchema):
+    class Meta:
+        model = UkStaffLocation
+        fields = [
+            "code",
+            "name",
+            "organisation",
+            "building_name",
         ]
