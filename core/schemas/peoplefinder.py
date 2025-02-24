b9512989362a0ff92df6123ac2daca565d3c6594
Oyeniyi Oyelade <oyeniyi.oyelade@digital.trade.gov.uk>
2025-02-24 13:41:25 +0000

    get people finder API, endpoint and schema with wiring into core and profile services

--- core/schemas/peoplefinder.py
+++ core/schemas/peoplefinder.py
@@ -3,10 +3,7 @@ from typing import Optional
 
 from ninja import Schema
 
-
-@dataclass
-class User:
-    sso_email_id: str
+from profiles.models import PeopleFinderProfile
 
 
 @dataclass
@@ -21,12 +18,12 @@ class MinimalPeopleFinderProfile(Schema):
     slug: str
     first_name: str
     last_name: str
-    user: Optional[User]  # ForeignKey to "user.User"
+    sso_email_id: str
     preferred_first_name: Optional[str]
     pronouns: Optional[str]
     name_pronunciation: Optional[str]
-    email: Optional[Email]  # ForeignKey to "profiles.Email"
-    contact_email: Optional[Email]  # ForeignKey to "profiles.Email"
+    email: Optional[str]
+    contact_email: Optional[str]
     primary_phone_number: Optional[str]
     secondary_phone_number: Optional[str]
     photo: Optional[str]  # ImageField is represented as a string (file path or URL)
@@ -34,3 +31,19 @@ class MinimalPeopleFinderProfile(Schema):
         str
     ]  # ImageField is represented as a string (file path or URL)
     grade: Optional[str]
+
+    @staticmethod
+    def resolve_slug(obj: PeopleFinderProfile):
+        return str(obj.slug)
+
+    @staticmethod
+    def resolve_sso_email_id(obj: PeopleFinderProfile):
+        return obj.user.sso_email_id
+
+    @staticmethod
+    def resolve_email(obj: PeopleFinderProfile):
+        return obj.email.address
+
+    @staticmethod
+    def resolve_contact_email(obj: PeopleFinderProfile):
+        return obj.contact_email.address
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
+from profiles.models import PeopleFinderProfile
 from profiles.models.combined import Profile
-from profiles.models.generic import Email
+from profiles.models.generic import Country, Email, UkStaffLocation
 from profiles.models.staff_sso import StaffSSOProfile, StaffSSOProfileEmail
 
 
@@ -10,3 +11,6 @@ admin.site.register(Email)
 admin.site.register(StaffSSOProfile)
 admin.site.register(StaffSSOProfileEmail)
 admin.site.register(Profile)
+admin.site.register(UkStaffLocation)
+admin.site.register(Country)
+admin.site.register(PeopleFinderProfile)
