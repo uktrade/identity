eda17001edd70231eb01e21581a90522ee3a5bdb
Ryan Williams <ryan.williams2@digital.trade.gov.uk>
2025-03-19 11:23:41 +0000

    make task non-binding

--- core/tasks.py
+++ core/tasks.py
@@ -2,6 +2,6 @@ from config.celery import celery_app
 from core.utils import CountriesS3Ingest
 
 
-@celery_app.task(bind=True)
-def ingest_countries(self):
+@celery_app.task()
+def ingest_countries():
     CountriesS3Ingest()
