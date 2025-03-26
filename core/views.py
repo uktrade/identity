f3b05e5907e956766f4b2cb67d3f02100758978f
marcelkornblum <me@marcelkornblum.com>
2025-03-26 11:35:35 +0000

    basic photo proxy

--- core/views.py
+++ core/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.shortcuts import render, HttpResponse
+import mimetypes
+
+from core import services
+from profiles.models.peoplefinder import PeopleFinderProfile
+
+
+@login_required
+def get_profile_photo(request, slug: str):
+    try:
+        profile = services.get_peoplefinder_profile_by_slug(slug=slug)
+    except PeopleFinderProfile.DoesNotExist:
+        raise Http404()
+
+    if not bool(profile.photo):
+        return HttpResponse("")
+
+    image = profile.photo.open()
+    response = HttpResponse(content=image)
+    response['Content-Type'] = str(mimetypes.guess_type(image.url)[0])
+    response['Content-Length'] = image.size
+    return response
 
 
 def index(request):
