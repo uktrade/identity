7140e45d840b0bbd55ae6e64f42836c5286cbf4f
Marcel Kornblum <marcelkornblum@users.noreply.github.com>
2025-03-26 13:24:30 +0000

    IDPF-377 basic photo proxy (#140)

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
+    path(route="profiles/<str:slug>/photo", view=views.get_profile_photo, name="photo"),
     path("sentry-debug/", views.trigger_error, name="sentry-debug"),
 ]
