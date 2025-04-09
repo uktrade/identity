a18a654fa25c6f05ddd6afd99f8b99fd29f9b8c8
marcelkornblum <me@marcelkornblum.com>
2025-04-09 14:17:36 +0100

    test coverage for the existing working utils

--- profiles/tests/peoplefinder/test_services_image.py
+++ profiles/tests/peoplefinder/test_services_image.py
@@ -1,5 +1,6 @@
 import pytest
 
+from profiles import types
 from profiles.services import image
 
 pytestmark = pytest.mark.django_db
@@ -10,12 +11,18 @@ def test_get_standard_profile_photo():
 
 
 def test_get_main_profile_photo():
-    assert False
+    mock_std_profile = mocker.patch("profiles.services.image.get_standard_profile_photo")
+    image.get_thumbnail_profile_photo(slug="a-slug")
+    mock_std_profile.assert_called_once_with(slug="a-slug", standard_size=types.ImageStandardSizes.MAIN)
 
 
 def test_get_small_profile_photo():
-    assert False
+    mock_std_profile = mocker.patch("profiles.services.image.get_standard_profile_photo")
+    image.get_thumbnail_profile_photo(slug="a-slug")
+    mock_std_profile.assert_called_once_with(slug="a-slug", standard_size=types.ImageStandardSizes.SMALL)
 
 
-def test_get_thumbnail_profile_photo():
-    assert False
+def test_get_thumbnail_profile_photo(mocker):
+    mock_std_profile = mocker.patch("profiles.services.image.get_standard_profile_photo")
+    image.get_thumbnail_profile_photo(slug="a-slug")
+    mock_std_profile.assert_called_once_with(slug="a-slug", standard_size=types.ImageStandardSizes.THUMBNAIL)
